e38f620d5b440050ea00b1adf27828634ef15bdc
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-03-25 23:25:41 +0000

    WIP

---
+++ index.js
@@ -0,0 +1,26 @@
+exports.handler = (event, context, callback) => {
+    const https = require("https");
+    const http = require("http");
+
+};
+
+
+
+// Helper functions
+buildSpeechletResponse = (outputText, shouldEndSession) => {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: outputText
+        },
+        shouldEndSession: shouldEndSession
+    }
+};
+
+generateResponse = (speechletResponse, sessionAttributes) => {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    }
+};

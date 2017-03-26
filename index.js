cf5bc7a87af3212b3219e794304db3883143ffe8
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-03-26 02:14:51 +0100

    WIp

--- index.js
+++ index.js
@@ -2,6 +2,87 @@ exports.handler = (event, context, callback) => {
     const https = require("https");
     const http = require("http");
 
+
+    console.log(event);
+
+
+    try {
+
+        if (event.session.new) {
+            // New session
+            console.log("New session");
+        }
+
+        console.log("Type: ", event.request.type);
+
+        switch (event.request.type) {
+            case "LaunchRequest": {
+                // Launch request
+                console.log("Launch request");
+                generateResponse(
+                    buildSpeechletResponse("Hello world", false),
+                    {}
+                );
+                break;
+            }
+
+
+
+
+
+
+            case "IntentRequest": {
+                // Intent request
+                console.log("Intent request");
+                console.log(event.request.intent.name);
+
+                switch (event.request.intent.name) {
+                    case "PenguinLocation" : {
+                        context.succeed (
+                            generateResponse(
+                                buildSpeechletResponse("Penguinsss", true),
+                                {}
+                            )
+                        );
+
+                        break;
+                    }
+
+                    default : {
+                        endSessionHandler("Default hit: Intent");
+                    }
+                }
+
+
+                break;
+            }
+
+            case "SessionEndRequest": {
+                // Session end request
+                break;
+            }
+
+            default: {
+                endSessionHandler("Default hit");
+            }
+        }
+
+    } catch(error) {
+        endSessionHandler("Catch hit");
+        // context.fail("Failed due to some unknown error");
+    }
+
+    function endSessionHandler(speech) {
+        if (typeof speech === "undefined") {
+            speech = "Good bye from the penguin finder.";
+        }
+        context.succeed(
+            generateResponse(
+                buildSpeechletResponse(speech, true),
+                {}
+            )
+        );
+    }
 };
 
 
